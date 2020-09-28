ff8aba512eb3b02b1973bf8c41575ac744526194
LokiSharp <loki.sharp@gmail.com>
2020-09-28 14:18:48 +0800

    test: 更新集成测试

--- test/integration/helper.ts
+++ test/integration/helper.ts
@@ -7,7 +7,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
 const { readFileSync } = require("fs");
-const { ScreepsServer, stdHooks } = require("screeps-server-mockup");
+const { ScreepsServer, stdHooks, TerrainMatrix } = require("screeps-server-mockup");
 const DIST_MAIN_JS = "dist/main.js";
 
 /*
@@ -34,8 +34,29 @@ class IntegrationTestHelper {
     // reset world but add invaders and source keepers bots
     await this._server.world.reset();
 
+    const terrain = new TerrainMatrix();
+    const walls = [
+      [10, 10],
+      [10, 40],
+      [40, 10],
+      [40, 40]
+    ];
+    _.each(walls, ([x, y]) => terrain.set(x, y, "wall"));
+
     // create a stub world composed of 9 rooms with sources and controller
-    await this._server.world.stubWorld();
+    await this._server.world.addRoom("W0N1");
+    await this._server.world.setTerrain("W0N1", terrain);
+    await this._server.world.addRoomObject("W0N1", "controller", 10, 10, { level: 0 });
+    await this._server.world.addRoomObject("W0N1", "source", 10, 40, {
+      energy: 1000,
+      energyCapacity: 1000,
+      ticksToRegeneration: 300
+    });
+    await this._server.world.addRoomObject("W0N1", "mineral", 40, 40, {
+      mineralType: "X",
+      density: 3,
+      mineralAmount: 3000
+    });
 
     // add a player with the built dist/main.js file
     const modules = {
--- test/integration/integration.test.ts
+++ test/integration/integration.test.ts
@@ -5,17 +5,26 @@ import { assert } from "chai";
 import { helper } from "./helper";
 
 describe("main", () => {
-  it("runs a server and matches the game tick", async () => {
+  it("测试服务器的 tick 是否匹配", async () => {
     for (let i = 1; i < 10; i += 1) {
       assert.equal(await helper.server.world.gameTime, i);
       await helper.server.tick();
     }
   });
 
-  it("writes and reads to memory", async function () {
+  it("测试是否能读写内存", async function () {
     await helper.player.console(`Memory.foo = 'bar'`);
     await helper.server.tick();
     const memory = JSON.parse(await helper.player.memory);
     assert.equal(memory.foo, "bar");
+  });
+
+  it("运行代码 1000 tick", async () => {
+    for (let i = 1; i < 1000; i += 1) {
+      console.log("[tick]", await helper.server.world.gameTime);
+      await helper.server.tick();
+      _.each(await helper.player.newNotifications, ({ message }) => console.log("[notification]", message));
+      console.log("[memory]", await helper.player.memory, "\n");
+    }
   });
 });
