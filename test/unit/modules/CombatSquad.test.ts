36ed38f8b07d2ea2792dc4d70ca097a6f23ebe44
LokiSharp <loki.sharp@gmail.com>
2020-11-26 00:52:09 +0800

    test: 重写 CombatSquad 单元测试

--- test/unit/modules/CombatSquad.test.ts
+++ test/unit/modules/CombatSquad.test.ts
@@ -5,24 +5,30 @@ import { assert } from "chai";
 
 describe("checkFormation", () => {
   it("检查阵型正确时返回 False", () => {
-    const squad: SquadMember = {
-      "↖": (new CreepMock("" as Id<CreepMock>, 0, 0) as unknown) as Creep,
-      "↗": (new CreepMock("" as Id<CreepMock>, 1, 0) as unknown) as Creep,
-      "↙": (new CreepMock("" as Id<CreepMock>, 0, 1) as unknown) as Creep,
-      "↘": (new CreepMock("" as Id<CreepMock>, 1, 1) as unknown) as Creep
-    };
+    const squadMembersDef: (string | number[] | Record<string, any[]>[])[][] = [
+      ["↖", [0, 0], []],
+      ["↗", [1, 0], []],
+      ["↙", [0, 1], []],
+      ["↘", [1, 1], []]
+    ];
+    const squad: SquadMember = {};
+    initSquadMember(squad, squadMembersDef);
+
     const result = CombatSquad.checkFormation((squad as unknown) as SquadMember);
 
     assert.isFalse(result);
   });
 
   it("检查阵型错误时返回 True", () => {
-    const squad: SquadMember = {
-      "↖": (new CreepMock("" as Id<CreepMock>, 2, 2) as unknown) as Creep,
-      "↗": (new CreepMock("" as Id<CreepMock>, 1, 0) as unknown) as Creep,
-      "↙": (new CreepMock("" as Id<CreepMock>, 0, 1) as unknown) as Creep,
-      "↘": (new CreepMock("" as Id<CreepMock>, 1, 1) as unknown) as Creep
-    };
+    const squadMembersDef: (string | number[] | Record<string, any[]>[])[][] = [
+      ["↖", [2, 2], []],
+      ["↗", [1, 0], []],
+      ["↙", [0, 1], []],
+      ["↘", [1, 1], []]
+    ];
+    const squad: SquadMember = {};
+    initSquadMember(squad, squadMembersDef);
+
     const result = CombatSquad.checkFormation((squad as unknown) as SquadMember);
 
     assert.isTrue(result);
@@ -34,34 +40,58 @@ describe("regroup", () => {
     ConstantsMock();
   });
   it("都在正确位置时返回 True", () => {
-    const squad: SquadMember = {
-      "↖": (new CreepMock("" as Id<CreepMock>, 0, 0) as unknown) as Creep,
-      "↗": (new CreepMock("" as Id<CreepMock>, 1, 0) as unknown) as Creep,
-      "↙": (new CreepMock("" as Id<CreepMock>, 0, 1) as unknown) as Creep,
-      "↘": (new CreepMock("" as Id<CreepMock>, 1, 1) as unknown) as Creep
-    };
+    const squadMembersDef: (string | number[] | Record<string, any[]>[])[][] = [
+      ["↖", [0, 0], []],
+      ["↗", [1, 0], []],
+      ["↙", [0, 1], []],
+      ["↘", [1, 1], []]
+    ];
+    const squad: SquadMember = {};
+    initSquadMember(squad, squadMembersDef);
+
     const result = CombatSquad.regroup((squad as unknown) as SquadMember);
 
     assert.isTrue(result);
-    assert.deepEqual(((squad["↖"] as unknown) as CreepMock).called, []);
-    assert.deepEqual(((squad["↗"] as unknown) as CreepMock).called, []);
-    assert.deepEqual(((squad["↙"] as unknown) as CreepMock).called, []);
-    assert.deepEqual(((squad["↘"] as unknown) as CreepMock).called, []);
+    assertSquadMember(squad, squadMembersDef);
   });
 
   it("位置不对时返回 False", () => {
-    const squad: SquadMember = {
-      "↖": (new CreepMock("" as Id<CreepMock>, 0, 0) as unknown) as Creep,
-      "↗": (new CreepMock("" as Id<CreepMock>, 1, 1) as unknown) as Creep,
-      "↙": (new CreepMock("" as Id<CreepMock>, 2, 2) as unknown) as Creep,
-      "↘": (new CreepMock("" as Id<CreepMock>, 3, 3) as unknown) as Creep
-    };
+    const squadMembersDef: (string | number[] | Record<string, any[]>[])[][] = [
+      ["↖", [0, 0], []],
+      ["↗", [1, 1], [{ moveTo: [1, 0, { reusePath: 1 }] }]],
+      ["↙", [2, 2], [{ moveTo: [0, 1, { reusePath: 1 }] }]],
+      ["↘", [3, 3], [{ moveTo: [1, 1, { reusePath: 1 }] }]]
+    ];
+    const squad: SquadMember = {};
+    initSquadMember(squad, squadMembersDef);
+
     const result = CombatSquad.regroup((squad as unknown) as SquadMember);
 
     assert.isFalse(result);
-    assert.deepEqual(((squad["↖"] as unknown) as CreepMock).called, []);
-    assert.deepEqual(((squad["↗"] as unknown) as CreepMock).called, [{ moveTo: [1, 0, { reusePath: 1 }] }]);
-    assert.deepEqual(((squad["↙"] as unknown) as CreepMock).called, [{ moveTo: [0, 1, { reusePath: 1 }] }]);
-    assert.deepEqual(((squad["↘"] as unknown) as CreepMock).called, [{ moveTo: [1, 1, { reusePath: 1 }] }]);
+    assertSquadMember(squad, squadMembersDef);
   });
 });
+
+function initSquadMember(
+  squad: SquadMember,
+  squadMembersDef: (string | number[] | Record<string, any[]>[])[][]
+): SquadMember {
+  squadMembersDef.forEach(
+    item =>
+      (squad[item[0] as string] = (new CreepMock(
+        "" as Id<CreepMock>,
+        item[1][0] as number,
+        item[1][1] as number
+      ) as unknown) as Creep)
+  );
+  return squad;
+}
+
+function assertSquadMember(
+  squad: SquadMember,
+  squadMembersDef: (string | number[] | Record<string, any[]>[])[][]
+): void {
+  squadMembersDef.forEach(item =>
+    assert.deepEqual(((squad[item[0] as string] as unknown) as CreepMock).called, item[2] as Record<string, any[]>[])
+  );
+}
