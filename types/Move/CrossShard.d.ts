6f53a0cf0e71c1e58f174a7b322041adcf11e19a
LokiSharp <loki.sharp@gmail.com>
2020-12-13 00:25:55 +0800

    refactor: 优化跨 shard 类型推导

--- types/Move/CrossShard.d.ts
+++ types/Move/CrossShard.d.ts
@@ -1,63 +1,111 @@
-// 目前官服存在的所有 shard 的名字
+/**
+ * 目前官服存在的所有 shard 的名字
+ */
 type ShardName = "shard0" | "shard1" | "shard2" | "shard3";
-// 跨 shard 请求构造器
-interface CrossShardRequestConstructor<RequestType, RequestData> {
-  // 要处理请求的 shard
+
+/**
+ * 跨 shard 请求构造器
+ */
+interface CrossShardRequestConstructor<K extends CrossShardRequestType> {
+  /**
+   * 要处理请求的 shard
+   */
   to: ShardName;
-  // 该请求的类型
-  type: RequestType;
-  // 该请求携带的数据
-  data: RequestData;
+  /**
+   * 该请求的类型
+   */
+  type: K;
+  /**
+   * 该请求携带的数据
+   */
+  data: CrossShardDatas[K];
 }
+
 /**
- * 跨 shard 请求 - 发送 creep
+ * 所有的跨 shard 数据（键为对应的信息 type）
  */
-type SendCreep = "sendCreep";
-interface SendCreepData {
-  // 要发送 creep 的名字
-  name: string;
-  // 要发送 creep 的内存
-  memory: CreepMemory | PowerCreepMemory;
+interface CrossShardDatas {
+  /**
+   * 跨 shard 请求 - 发送 creep
+   */
+  sendCreep: {
+    /**
+     * 要发送 creep 的名字
+     */
+    name: string;
+    /**
+     * 要发送 creep 的内存
+     */
+    memory: CreepMemory;
+  };
+  /**
+   * 跨 shard 请求 - 提交重新孵化任务
+   */
+  sendRespawn: {
+    /**
+     * 要重新孵化的 creep 的名字
+     */
+    name: string;
+    /**
+     * 要重新孵化的 creep 的内存
+     */
+    memory: CreepMemory | PowerCreepMemory;
+  };
 }
+
 /**
- * 跨 shard 请求 - 提交重新孵化任务
+ * 所有的跨 shard 请求
  */
-type SendRespawn = "sendRespawn";
-interface SendRespawnData {
-  // 要重新孵化的 creep 的名字
-  name: string;
-  // 要重新孵化的 creep 的内存
-  memory: CreepMemory | PowerCreepMemory;
-}
-// 构造所有的跨 shard 请求
-type CrossShardRequest =
-  | CrossShardRequestConstructor<SendCreep, SendCreepData>
-  | CrossShardRequestConstructor<SendRespawn, SendRespawnData>;
-// 所有跨 shard 请求的类型和数据
-type CrossShardRequestType = SendCreep | SendRespawn;
-type CrossShardRequestData = SendCreepData | SendRespawnData;
-// 所有跨 shard 请求的执行策略
+type CrossShardRequest = CrossShardRequestConstructor<CrossShardRequestType>;
+
+/**
+ * 所有跨 shard 请求的类型
+ */
+type CrossShardRequestType = keyof CrossShardDatas;
+
+/**
+ * 所有跨 shard 请求的执行策略
+ */
 type CrossShardRequestStrategies = {
-  [type in CrossShardRequestType]: (data: CrossShardRequestData) => ScreepsReturnCode;
+  [type in CrossShardRequestType]: (data: CrossShardDatas[type]) => ScreepsReturnCode;
 };
-// 镜面的跨 shard 数据
+
+/**
+ * 镜面的跨 shard 数据
+ */
 type InterShardData = {
   [shard in ShardName]?: {
-    // 一个键值对构成了一个消息
+    /**
+     * 一个键值对构成了一个消息
+     */
     [msgName: string]: CrossShardRequest | ScreepsReturnCode;
   };
 };
-// 跨 shard 响应
+
+/**
+ * 跨 shard 响应
+ */
 interface CrossShardReply {
-  // 响应是否存在
+  /**
+   * 响应是否存在
+   */
   has: boolean;
-  // 响应的状态
+  /**
+   * 响应的状态
+   */
   result?: ScreepsReturnCode;
 }
-// 跨 shard 请求的元数据
+
+/**
+ * 跨 shard 请求的元数据
+ */
 interface CrossShardRequestInfo {
-  // 请求的发送 shard
+  /**
+   * 请求的发送 shard
+   */
   source: ShardName;
-  // 请求的名称
+  /**
+   * 请求的名称
+   */
   name: string;
 }
