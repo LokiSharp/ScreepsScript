0e3f7142f87ce52daf0ef43a3c37bad5626f6063
LokiSharp <loki.sharp@gmail.com>
2020-12-16 10:23:40 +0800

    test: 移动 RCLTest 位置和文件后缀，与其他测试区分

--- test/integration/RCLTest/RCL1ToRCL2.RCLTest.ts
+++ test/integration/RCLTest/RCL1ToRCL2.RCLTest.ts
@@ -1,8 +1,8 @@
-import { runRCLTest } from "./utils/runRCLTest";
+import { runRCLTest } from "../utils/runRCLTest";
 const TICK_NUM = 10000;
 const RCL = 1;
 
-describe("main", () => {
+describe(`测试 RCL${RCL} -> RCL${RCL + 1}`, () => {
   it(`测试 RCL${RCL} -> RCL${RCL + 1}`, async () => {
     await runRCLTest(RCL, RCL + 1, TICK_NUM);
   });
--- test/integration/RCLTest/RCL7ToRCL8.RCLTest.ts
+++ test/integration/RCLTest/RCL7ToRCL8.RCLTest.ts
@@ -1,8 +1,8 @@
-import { runRCLTest } from "./utils/runRCLTest";
+import { runRCLTest } from "../utils/runRCLTest";
 const TICK_NUM = 100000;
-const RCL = 3;
+const RCL = 7;
 
-describe("main", () => {
+describe(`测试 RCL${RCL} -> RCL${RCL + 1}`, () => {
   it(`测试 RCL${RCL} -> RCL${RCL + 1}`, async () => {
     await runRCLTest(RCL, RCL + 1, TICK_NUM);
   });
