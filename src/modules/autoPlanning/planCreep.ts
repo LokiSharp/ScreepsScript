8829e9a7188f193d6a2a7d184c2f144b20929aa4
LokiSharp <loki.sharp@gmail.com>
2021-02-28 16:52:14 +0800

    perf: 调整 upgrader 数量

--- src/modules/autoPlanning/planCreep.ts
+++ src/modules/autoPlanning/planCreep.ts
@@ -199,7 +199,7 @@ const releasePlans: CreepReleasePlans = {
       // 兜底，从 sourceContainer 中获取能量
       ({ room, sourceContainerIds }: UpgraderPlanStats) => {
         // 有援建单位，每个 container 少发布一个 upgrader
-        const upgraderIndexs = creepApi.has(`${room.name} RemoteUpgrader`) ? [0, 1, 2] : [0, 1, 2, 3];
+        const upgraderIndexs = creepApi.has(`${room.name} RemoteUpgrader`) ? [0] : [0, 1, 2];
 
         // 遍历所有 container，发布对应数量的 upgrader
         sourceContainerIds.forEach((containerId, index) => {
