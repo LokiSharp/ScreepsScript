fed2b8edbc8ade3d33310c98cbed4f7d43b6e750
LokiSharp <loki.sharp@gmail.com>
2021-04-21 21:36:34 +0800

    ci: 所有分支都运行 CI

--- src/modules/room/spawn/CreepRelease.ts
+++ src/modules/room/spawn/CreepRelease.ts
@@ -384,7 +384,7 @@ export default class RoomCreepReleaseController {
         name: rangedAttackerName,
         role: "rangedAttacker",
         data: {
-          targetFlagName: targetFlagName,
+          targetFlagName,
           creepName: rangedAttackerName,
           keepSpawn,
           wayPoint
